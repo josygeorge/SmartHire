c99278870411b153b8797228b0cf727a785e6069
Josy George <joe.george.josy@gmail.com>
2025-07-10 16:54:26 -0400

    config.ts updated for both local+electron and production- backend Render API

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,4 +1,6 @@
+// src/config/config.ts
+
 export const BASE_URL =
-  window.location.protocol === 'file:' // Electron or static build
-    ? 'http://localhost:5050' // Your local backend API [for development purposes only]. Change to actual production URL in production.
-    : import.meta.env.VITE_API_BASE_URL || 'http://localhost:5050';
+  window.location.protocol === 'file:' // Electron or local static build
+    ? 'http://localhost:5050'
+    : import.meta.env.VITE_API_BASE_URL!;
