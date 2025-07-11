6d73bea04f2954e6c93f6e860a0d313f9b858e86
Josy George <joe.george.josy@gmail.com>
2025-07-10 20:55:57 -0400

    TroubleShooting:corrected .d.ts file- types and build issues for Render deployment

--- backend/src/types/custom-express.d.ts
+++ backend/src/types/custom-express.d.ts
@@ -1,7 +1,8 @@
 // src/types/express/index.d.ts
 // This file extends the Express Request interface to include a user object
 declare namespace Express {
-  export interface Request {
+  /* export interface Request { */ // This is the correct way to extend the Request interface in Express
+  interface Request {
     user?: {
       id: string;
       role: 'admin' | 'user';
